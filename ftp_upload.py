c5e99d4241bfc9b065c632b816dd9a4e39c014d5
ratisil <ratisil@live.com>
2025-03-19 11:00:02 +0700

    Update ftp_upload.py

--- ftp_upload.py
+++ ftp_upload.py
@@ -7,7 +7,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
 
-# We'll output to stdout only, or you can add a file handler if desired
+# Output logs to stdout
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
@@ -26,6 +26,16 @@ def ftp_upload(local_filepath, remote_filename):
         ftp = FTP()
         ftp.connect(ftp_host, ftp_port)
         ftp.login(user=ftp_user, passwd=ftp_pass)
+        # Log the current FTP directory
+        current_dir = ftp.pwd()
+        logger.info(f"Current FTP directory: {current_dir}")
+        
+        # If needed, change directory:
+        # target_dir = os.environ.get("FTP_TARGET_DIR", "")
+        # if target_dir:
+        #     ftp.cwd(target_dir)
+        #     logger.info(f"Changed FTP directory to: {ftp.pwd()}")
+        
         with open(local_filepath, "rb") as f:
             ftp.storbinary(f"STOR " + remote_filename, f)
         logger.info(f"Uploaded {local_filepath} to FTP as {remote_filename}.")
