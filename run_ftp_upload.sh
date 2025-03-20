08d095960c9d04d3372dd0ec5cfec0ff2525854a
ratisil <ratisil@live.com>
2025-03-20 10:36:08 +0700

    fixing cron again

--- run_ftp_upload.sh
+++ run_ftp_upload.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo "run_ftp_upload.sh triggered at $(date)" >> /app/logs/cron_debug.txt
+
 # Export the FTP environment variables so that they are available to the script.
 export FTP_HOST="${FTP_HOST}"
 export FTP_PORT="${FTP_PORT}"
 export FTP_USER="${FTP_USER}"
 export FTP_PASS="${FTP_PASS}"
+
 cd /app
 /usr/local/bin/python ftp_upload.py /app/logs/fpl_prices.html fpl_prices.html
