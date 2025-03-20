aa8992457e7e74a2e3e910de6b4957f179955ffc
ratisil <ratisil@live.com>
2025-03-20 11:00:46 +0700

    back to working yesterday

--- ftp_upload.py
+++ ftp_upload.py
@@ -7,15 +7,12 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
 
-# Output to stdout
+# We'll output to stdout only, or you can add a file handler if desired
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
 def ftp_upload(local_filepath, remote_filename):
-    logger.info("FTP upload job triggered.")
-    logger.info(f"Arguments received: local_file = {local_filepath}, remote_file = {remote_filename}")
-    
     ftp_host = os.environ.get("FTP_HOST")
     ftp_port = int(os.environ.get("FTP_PORT"))
     ftp_user = os.environ.get("FTP_USER")
@@ -29,10 +26,8 @@ def ftp_upload(local_filepath, remote_filename):
         ftp = FTP()
         ftp.connect(ftp_host, ftp_port)
         ftp.login(user=ftp_user, passwd=ftp_pass)
-        current_dir = ftp.pwd()
-        logger.info(f"Current FTP directory: {current_dir}")
         with open(local_filepath, "rb") as f:
-            ftp.storbinary("STOR " + remote_filename, f)
+            ftp.storbinary(f"STOR " + remote_filename, f)
         logger.info(f"Uploaded {local_filepath} to FTP as {remote_filename}.")
         ftp.quit()
     except Exception as e:
